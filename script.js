01988d21b384afe4643e3e44f2f2753b960aabc4
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-19 20:52:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
 
+let getList = document.getElementById("infi-list");
+
+let getMain = document.querySelector("main");
+
+let num = 10;
+
+function triggerThis(num, i) {
+  console.log(i);
+  for (let m = i; m <= num; m++) {
+    let addList = document.createElement("li");
+    addList.innerText = `Item ${m}`;
+    getList.appendChild(addList);
+  }
+}
+
+triggerThis(num, 1);
+
+getList.addEventListener("scrollend", function () {
+  num = num + 2;
+  triggerThis(num, num - 1);
+});
